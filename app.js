af169a2762a9be51bbb0cb1266497b130079e81b
Zhurin Sergey <zWX889616@china.huawei.com>
2020-04-10 08:27:34 +0700

    init commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+  
+const app = express();
+
+require('./app/routes')(app, {});
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('Start server');
+});
+
+/*
+app.get("/", function(request, response){
+      
+    response.send("<h1>Главная страница</h1>");
+});
+app.use("/about", function(request, response){
+      
+    let id = request.query.id;
+    let userName = request.query.name;
+    response.send("<h1>Информация</h1><p>id=" + id +"</p><p>name=" + userName + "</p>");
+});
+ 
+app.listen(3000); */
