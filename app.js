5d977af4af7370e2579bccccf095d1202c4047d1
Zhurin Sergey <zWX889616@china.huawei.com>
2020-04-10 11:18:46 +0700

    add api method for get all tokens

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const url = "mongodb://localhost:27017/";
-MongoClient.connect(url, (err, db) => {
-	if (err) 
-		return console.log(err);
+MongoClient.connect(url, (err, client) => {
+	if (err) return console.log(err);
 	  
-	require('./app/routes')(app, db);
+	require('./app/routes')(app, client.db('api'));
 	
 	app.listen(3000, () => {
 		console.log('Server start');
--- app/routes/token_routes.js
+++ app/routes/token_routes.js
@@ -1,3 +1,8 @@
 module.exports = function(app, db) {
-
+	app.get('/tokens', (req, res) => {
+		db.collection('tokens').find().toArray((err, result) => {
+			if (err) throw err;
+			res.send(result);
+		});
+	});
 };
