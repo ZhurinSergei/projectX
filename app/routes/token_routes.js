8bda9820ef5c5ae9001e1a023616bd9a59c0b4d5
Zhurin Sergey <zWX889616@china.huawei.com>
2020-04-10 12:19:33 +0700

    changed api method for get all tokens

--- app/routes/token_routes.js
+++ app/routes/token_routes.js
@@ -1,6 +1,9 @@
 module.exports = function(app, db) {
 	app.get('/tokens', (req, res) => {
-		db.collection('tokens').find().toArray((err, result) => {
+		const status = req.query['status'];	
+		const details = status != undefined ? {status: status} : {};
+
+		db.collection('tokens').find(details).toArray((err, result) => {
 			if (err) throw err;
 			res.send(result);
 		});
